c5eeb0ec6312e2d88b8307a9f5d682f802784192
JohnQuentin <67411883+JohnQuentin@users.noreply.github.com>
2021-08-16 16:15:20 +0200

    Create Fake data.py

---
+++ Fake data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+data = {'Name':['Noah', 'Emma', 'Sophia', 'Robert'],
+        'Age':[43, 23, 43, 12],
+        'Height' : [184, 156, 166, 161]
+        }
+
+df = pd.DataFrame(data)
